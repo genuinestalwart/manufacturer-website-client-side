e12a2c154095ae8ab65d1d9637fa7ecba1f95b2f
genuinestalwart <genuinestalwart@gmail.com>
2022-05-22 20:13:28 +0600

    banner added

---
+++ src/components/Home/Banner.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import bannar from '../../assets/banner.jpg';
+
+const Banner = () => {
+    return (
+        <section className='px-20'>
+            <div className="hero h-[calc(100vh_-_5rem)]">
+                <div className="hero-content flex md:flex-row-reverse">
+                    <img src={bannar} className="w-1/2 rounded-lg shadow-2xl" alt='' />
+                    <div className='w-1/2'>
+                        <h1 className="text-5xl font-bold">Buy One, Win One!</h1>
+                        <p className="py-6">Following our old legacy, we have launched our best selling offer once again after the pandemic. So what are you waiting for? Let's dive in!</p>
+                        <button className="btn btn-primary font-bold text-accent">Learn More</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Banner;
--- src/components/Shared/Header/Header.js
+++ src/components/Shared/Header/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
         <header className='flex h-20 items-center justify-between px-16'>
             <div className='flex h-1/2'>
                 <div><img className='h-full' src={logo} alt="computer logo" /></div>
-                <h1 className='flex font-bold items-center mx-2 text-3xl text-primary'>Manufacture Online</h1>
+                <h2 className='flex font-bold items-center mx-2 text-3xl text-primary'>Manufacture Online</h2>
             </div>
 
             <Navbar></Navbar>
