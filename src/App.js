e1cdf7fd5d85b41d0eee43ec0654c3ea1a30d8bf
genuinestalwart <genuinestalwart@gmail.com>
2022-05-27 18:06:41 +0600

    dashboard structure is ready

--- src/App.js
+++ src/App.js
@@ -16,6 +16,13 @@ import LoadingSpinner from './components/Shared/Others/LoadingSpinner';
 import RequireAuth from './components/Shared/Require/RequireAuth';
 import auth from './firebase.init';
 import { ToastContainer } from 'react-toastify';
+import AddProduct from './components/Dashboard/AddProduct';
+import AddReview from './components/Dashboard/AddReview';
+import AllUsers from './components/Dashboard/AllUsers';
+import ManageOrders from './components/Dashboard/ManageOrders';
+import ManageProducts from './components/Dashboard/ManageProducts';
+import MyOrders from './components/Dashboard/MyOrders';
+import MyProfile from './components/Dashboard/MyProfile';
 
 function App() {
 	const [, uLoading] = useAuthState(auth);
@@ -32,7 +39,15 @@ function App() {
 							<Routes>
 								<Route path='/' element={<Home></Home>}></Route>
 								<Route path='/blogs' element={<Blogs></Blogs>}></Route>
-								<Route path='/dashboard' element={<RequireAuth><Dashboard></Dashboard></RequireAuth>}></Route>
+								<Route path='/dashboard' element={<RequireAuth><Dashboard></Dashboard></RequireAuth>}>
+									<Route path='add-product' element={<AddProduct></AddProduct>}></Route>
+									<Route path='add-review' element={<AddReview></AddReview>}></Route>
+									<Route path='users' element={<AllUsers></AllUsers>}></Route>
+									<Route path='manage-orders' element={<ManageOrders></ManageOrders>}></Route>
+									<Route path='manage-products' element={<ManageProducts></ManageProducts>}></Route>
+									<Route path='orders' element={<MyOrders></MyOrders>}></Route>
+									<Route path='profile' element={<MyProfile></MyProfile>}></Route>
+								</Route>
 								<Route path='/login' element={<Login></Login>}></Route>
 								<Route path='/my-portfolio' element={<MyPortfolio></MyPortfolio>}></Route>
 								<Route path='/purchase' element={<RequireAuth><Purchase></Purchase></RequireAuth>}></Route>
--- src/components/Main/Dashboard.js
+++ src/components/Main/Dashboard.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
 
 const Dashboard = () => {
     return (
-        <div>
+        <section>
+            <div className="drawer h-[calc(100vh_-_5rem)]">
+                <input id="dashboard-drawer" type="checkbox" className="drawer-toggle" />
 
-        </div>
+                <div className='divide-x-2 divide-accent drawer-content flex'>
+                    <div className="flex justify-center w-16">
+                        <label htmlFor="dashboard-drawer" className="bg-base-200 hover:bg-base-300 btn border-0 drawer-button h-fit min-h-fit my-2 px-3 py-2 rounded-md text-secondary hover:text-primary"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg></label>
+                    </div>
+
+                    <div className='h-full overflow-y-auto px-4'><Outlet /></div>
+                </div>
+
+                <div className="drawer-side">
+                    <label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
+
+                    <ul className="menu p-4 overflow-y-auto w-1/4 bg-base-100 text-base-content">
+                        <li><Link to='/dashboard/orders'>My Orders</Link></li>
+                        <li><Link to='/dashboard/add-review'>Add a Review</Link></li>
+                        <li><Link to='/dashboard/profile'>My Profile</Link></li>
+                        <li><Link to='/dashboard/manage-orders'>Manage All Orders</Link></li>
+                        <li><Link to='/dashboard/add-product'>Add a Product</Link></li>
+                        <li><Link to='/dashboard/users'>All Users</Link></li>
+                        <li><Link to='/dashboard/manage-products'>Manage All Products</Link></li>
+                    </ul>
+                </div>
+            </div>
+        </section>
     );
 };
 
